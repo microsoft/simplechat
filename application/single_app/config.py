01fa3fa368de2a34f0ef628b0d45c64f330ccf73
Bionic711 <nadoyle@microsoft.com>
2025-12-17 11:05:40 -0600

    fix user input filesystem path vulns

--- application/single_app/config.py
+++ application/single_app/config.py
@@ -88,7 +88,7 @@ load_dotenv()
 EXECUTOR_TYPE = 'thread'
 EXECUTOR_MAX_WORKERS = 30
 SESSION_TYPE = 'filesystem'
-VERSION = "0.233.179"
+VERSION = "0.233.182"
 
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key-change-in-production')
