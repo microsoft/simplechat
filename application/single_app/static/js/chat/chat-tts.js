f68ce7649404e4989a655e206521315bd4d2d2ee
Paul Lizer <paullizer@microsoft.com>
2026-01-07 17:13:34 -0500

    Auto Voice Response

--- application/single_app/static/js/chat/chat-tts.js
+++ application/single_app/static/js/chat/chat-tts.js
@@ -619,12 +619,12 @@ export function updateAutoplayButton() {
     const icon = button.querySelector('i');
     if (ttsAutoplay) {
         icon.className = 'bi bi-volume-up-fill';
-        button.title = 'TTS Autoplay On - click to disable';
+        button.title = 'Auto Voice Response On - click to disable';
         button.classList.remove('btn-outline-secondary');
         button.classList.add('btn-primary');
     } else {
         icon.className = 'bi bi-volume-mute';
-        button.title = 'TTS Autoplay Off - click to enable';
+        button.title = 'Auto Voice Response Off - click to enable';
         button.classList.remove('btn-primary');
         button.classList.add('btn-outline-secondary');
     }
