a2bb4699b12d6c31c1102f7246cadb74629707e3
Paul Lizer <paullizer@microsoft.com>
2026-01-13 11:55:11 -0500

    Adding release notes

--- application/single_app/config.py
+++ application/single_app/config.py
@@ -88,7 +88,7 @@ load_dotenv()
 EXECUTOR_TYPE = 'thread'
 EXECUTOR_MAX_WORKERS = 30
 SESSION_TYPE = 'filesystem'
-VERSION = "0.234.225"
+VERSION = "0.235.001"
 
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key-change-in-production')
