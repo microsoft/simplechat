c5a5f0d41e77a053b117f866127f79bec1089137
SteveCInVA <stecarr@microsoft.com>
2026-03-04 19:34:22 +0000

    fix: update installation method for AZD in devContainer

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo rm -f /usr/local/bin/az
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update
+sudo apt-get install -y azure-cli
+python3 -m venv .venv
+.venv/bin/pip install -r application/single_app/requirements.txt
