d75dcbc9208b57f75773751296ae7db9e58ba348
Paul Lizer <paullizer@microsoft.com>
2026-05-27 20:17:35 -0400

    fixed min ai message width

--- application/single_app/config.py
+++ application/single_app/config.py
@@ -94,7 +94,7 @@ load_dotenv()
 EXECUTOR_TYPE = 'thread'
 EXECUTOR_MAX_WORKERS = 30
 SESSION_TYPE = 'filesystem'
-VERSION = "0.241.106"
+VERSION = "0.241.108"
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key-change-in-production')
 
